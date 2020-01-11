92ffc97de4167769aafd258ed5d1e08c68358fbc
Giuseppe Dinardo <giuseppe.dinardo67@gmail.com>
2020-01-11 15:32:55 +0100

    Removed unused code, refactored code and increased documentation. It is now possible to use the program through the command line

--- Program.cs
+++ Program.cs
@@ -1,6 +1,34 @@
 ﻿/*
-Datascraper for Google drive, after making all the files in you drive load, press F12, in the opened window 
-go to elements and copy the the whole html body tag of the web page and save it in a txt file
+
+MIT License
+
+Copyright (c) 2020 Alessandro Dinardo
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+.__          .     .__             .__     ,       __.                  
+[ __ _  _  _ | _   |  \._*.  , _   |  \ _.-+- _.  (__  _ ._ _.._  _ ._
+[_./(_)(_)(_]|(/,  |__/[ | \/ (/,  |__/(_] | (_]  .__)(_ [ (_][_)(/,[  
+          ._|                                                   |        
+
+    After making the files you want in your final list load(sometimes they'll load only by selecting them), press F12, go to the Elements tab in the opened window,
+    copy the Body element, paste it in a txt file and save it
 */
 
 using System;
@@ -10,30 +38,111 @@ using System.IO;
 using System.Text;
 using System.Threading;
 using System.Web;
-using System.Linq;
 
 namespace Google_Drive_data_scraper
 {
     class Program
     {
-
-        static string bodyTagString; //body tag as stringk
-        static char[] bodyTag; //body tag as array of characters
-        static int fileNameStart; //start of the name of a file
-        static int fileNameEnd; //end of the name of a file
-        static int fileNameLength; //length of the name of a file
-        static string fileName = ""; //name of a file
-        static int latestPos = 0; //latest position of the found file name
-        static StringBuilder sb = new StringBuilder();
+        static List<String> fileNames;
         static void Main(string[] args)
         {
+            #region Print logo
+            Console.WriteLine(".__          .     .__             .__     ,       __.                \n[ __ _  _  _ | _   |  \\._*.  , _   |  \\ _.-+- _.  (__  _ ._ _.._  _ ._\n[_./(_)(_)(_]|(/,  |__/[ | \\/ (/,  |__/(_] | (_]  .__)(_ [ (_][_)(/,[  \n          ._|                                                   |        \n");
+            for (int i = 1; i <= Console.BufferWidth; i++)
+            {
+                Console.Write("*");
+            }
+            #endregion
+
             string txtFilePath;
-            Console.Write("Drop the txt containing the body tag here or type its path: ");
-            txtFilePath = Console.ReadLine();
+            //if program is exectued by command line parse the arguments and set the txtFilePath
+            if (args.Length != 0 && args != null)
+            {
+                ParseAndExecuteArguments(args);
+                txtFilePath = args[0];
+            }
+            //ask the user for the path
+            else
+            {
+                Console.Write("\nDrop the txt containing the body tag here or type its path: ");
+                txtFilePath = Console.ReadLine();
+            }
+
+            StartScraping(txtFilePath);
+
+            #region Compares the files and prints the results
+            Console.Write("\nDo you want to compare the list of files to a local folder? (Type the path of the folder if yes, enter if not) \nPath: ");
+            string pathToFolder = Console.ReadLine();
+            if (pathToFolder != String.Empty)
+            {
+                if (Path.HasExtension(pathToFolder) || !Directory.Exists(pathToFolder))
+                {
+                    PrintError("Invalid path");
+                }
+                else
+                {
+                    (List<String> FromFolder, List<String> FromDrive) missingFiles = CompareFiles(pathToFolder, fileNames);
+
+                    if (missingFiles.FromDrive != null)
+                    {
+                        Console.WriteLine("\nThese files are missing from your Drive:");
+                        foreach (var file in missingFiles.FromDrive)
+                        {
+                            Console.WriteLine($"{missingFiles.FromDrive.IndexOf(file) + 1}) {file}");
+                        }
+                    }
+                    if (missingFiles.FromFolder != null)
+                    {
+                        Console.WriteLine($"\nThese files are missing from your folder {Path.GetDirectoryName(pathToFolder)}:");
+                        foreach (var file in missingFiles.FromFolder)
+                        {
+                            Console.WriteLine($"{missingFiles.FromFolder.IndexOf(file) + 1}) {file}");
+                        }
+                    }
+                    Console.ReadLine();
+                }
+            }
+            #endregion
+
+            #region Save a list of the files
+            Console.WriteLine("\nDo you want to export the list of the files in a txt file? It'll be saved on the desktop (Y/N)");
+            var resp = Console.ReadKey();
+            if (resp.KeyChar == 'n' && resp.KeyChar == 'N')
+                return;
+            SaveList();
+            #endregion
+
+            Environment.Exit(0);
+        }
+
+
+        /// <summary>
+        /// Parses the arguments passed through the command line and executes them
+        /// </summary>
+        /// <param name="arguments"> Arguments passed to the program</param>
+        static void ParseAndExecuteArguments(string[] arguments)
+        {
+            if (arguments[0] == "-scrape")
+            {
+                StartScraping(arguments[1]);
+            }
+            if (arguments[2] == "-save")
+            {
+                SaveList();
+            }
+            Environment.Exit(0);
+        }
+
+
+        /// <summary>
+        /// Starts the process of scraping the names of the files
+        /// </summary>
+        /// <param name="txtFilePath"> Path to the txt file which contains the Body tag </param>
+        static void StartScraping(string txtFilePath)
+        {
             //check if the path leads to a txt file
             if (!txtFilePath.Contains(".txt"))
             {
-                Console.ForegroundColor = ConsoleColor.Red;
                 PrintError("Invalid path, it doesn't lead to a txt file");
                 return;
             }
@@ -44,20 +153,12 @@ namespace Google_Drive_data_scraper
                 txtFilePath = buffTxtFilePath.Trim('"');
             }
 
-            //takes data from txt file and decodes it using the HttpUtility(or else for example '&' would be shown as '&amp;')
-            using (StreamReader sr = new StreamReader(File.OpenRead(txtFilePath)))
-            {
-                bodyTagString = sr.ReadToEnd();
-                bodyTagString = HttpUtility.HtmlDecode(bodyTagString);
-                bodyTag = bodyTagString.ToCharArray();
-            }
-
             Console.Write("Scraping data...");
             #region Scraping process      
-            List<String> fileNames = new List<string>();
+            fileNames = new List<string>();
             Thread thr = new Thread(() =>
             {
-                fileNames = FindFilesName();
+                fileNames = FindFilesName(txtFilePath);
             });
             thr.IsBackground = true;
             thr.Start();
@@ -75,87 +176,33 @@ namespace Google_Drive_data_scraper
                 Console.WriteLine($"{fileIndex}) {name}");
                 fileIndex++;
             }
-
-            #region Compares the files and prints the results
-            Console.Write("\nDo you want to compare the list of files to a local folder? (Type the path of the folder if yes, enter if not) \nPath: ");
-            string pathToFolder = Console.ReadLine();
-            if (pathToFolder != String.Empty)
-            {
-                if (Path.HasExtension(pathToFolder) || !Directory.Exists(pathToFolder))
-                {
-                    PrintError("Invalid path");
-                }
-                else
-                {
-                    (List<String> FromFolder , List<String> FromDrive) missingFiles = CompareFiles(pathToFolder, fileNames);
-
-                    if (missingFiles.FromDrive != null)
-                    {
-                        Console.WriteLine("\nThese files are missing from your Drive:");
-                        foreach (var file in missingFiles.FromDrive)
-                        {
-                            Console.WriteLine($"{missingFiles.FromDrive.IndexOf(file)+1}) {file}");
-                        }
-                    }
-                    if (missingFiles.FromFolder != null)
-                    {
-                        Console.WriteLine($"\nThese files are missing from your folder {Path.GetDirectoryName(pathToFolder)}:");
-                        foreach (var file in missingFiles.FromFolder)
-                        {
-                            Console.WriteLine($"{missingFiles.FromFolder.IndexOf(file)+1}) {file}");
-                        }
-                    }
-
-
-                    #region old compare
-                    // String location = CompareFiles(pathToFolder, fileNames).location;
-                    // List<String> missingFiles = CompareFiles(pathToFolder, fileNames).files;
-
-                    // if (missingFiles.Count > 0)
-                    // {
-                    //     if (location.ToLower() == "folder") { Console.WriteLine($"These files are missing from the folder {Path.GetDirectoryName(pathToFolder)}: "); }
-                    //     else
-                    //     {
-                    //         Console.WriteLine("These files are missing from your Drive: ");
-                    //     }
-                    //     foreach (var file in missingFiles)
-                    //     {
-                    //         Console.WriteLine($"{missingFiles.IndexOf(file) + 1}) {file}");
-                    //     }
-                    // }
-                    // else
-                    //     Console.WriteLine("There are no missing files");
-                    #endregion
-                    Console.ReadLine();
-                }
-            }
-            #endregion
-
-            #region Exports list
-            Console.WriteLine("\nDo you want to export the list of the files in a txt file? It'll be saved on the desktop (Y/N)");
-            var resp = Console.ReadKey();
-            if (resp.KeyChar != 'y' && resp.KeyChar != 'Y')
-            {
-                return;
-            }
-            string filesNameTxtPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\GoogleDriveFiles.txt";
-            using (StreamWriter sw = File.CreateText(filesNameTxtPath))
-            {
-                fileIndex = 1;
-                foreach (var name in fileNames)
-                {
-                    sw.WriteLine($"{fileIndex}) {name}");
-                    fileIndex++;
-                }
-            }
-            Process.Start(@"cmd.exe ", $"/c {Path.GetFullPath(filesNameTxtPath)}");
-            #endregion
-
-            Environment.Exit(0);
         }
 
-        private static List<String> FindFilesName()
+
+        /// <summary>
+        /// Finds the files in the user's Drive
+        /// </summary>
+        /// <param name="txtFilePath"> Path to the txt file which contains the Body tag</param>
+        /// <returns> Returns a List of type string which contains all the names of the files found in the Body tag</returns>
+        private static List<String> FindFilesName(string txtFilePath)
         {
+            int latestPos = 0; //latest position of the found file name
+            string bodyTagString; //body tag as string
+            char[] bodyTag; //body tag as array of characters
+            int fileNameStart; //start of the name of a file
+            int fileNameEnd; //end of the name of a file
+            int fileNameLength; //length of the name of a file
+            string fileName = ""; //name of a file
+            StringBuilder sb = new StringBuilder();
+
+            //takes data from txt file and decodes it using the HttpUtility(or else for example '&' would be shown as '&amp;')
+            using (StreamReader sr = new StreamReader(File.OpenRead(txtFilePath)))
+            {
+                bodyTagString = sr.ReadToEnd();
+                bodyTagString = HttpUtility.HtmlDecode(bodyTagString);
+                bodyTag = bodyTagString.ToCharArray();
+            }
+
             List<String> fileNames = new List<string>();
             for (int i = latestPos; i <= bodyTag.Length - 1; i++)
             {
@@ -193,11 +240,16 @@ namespace Google_Drive_data_scraper
             return fileNames;
         }
 
-        /* Compares files from the list to the files in a folder
-        *  Returns a tuple which contains:
-        *   - List<String> missingFilesFromFolder: files missing from the folder when compared to the drive
-        *   - List<String> missingFilesFromDrive: files missing from the drive when compared to the folder
-        */
+
+        /// <summary>
+        /// Compares files from the list to the files in a folder
+        /// </summary>
+        /// <param name="folderPath"> Folder that contains the files to compare to the ones in the user's Drive</param>
+        /// <param name="driveFiles"> A list which contains the files in the user's Drive. It is equal to the list of type string "fileNames"</param>
+        /// <returns> Returns a tuple which contains: 
+        /// - List<String> missingFilesFromFolder: files missing from the folder when compared to the drive
+        /// - List<String> missingFilesFromDrive: files missing from the drive when compared to the folder
+        /// </returns>
         private static (List<String> missingFilesFromFolder, List<String> missingFilesFromDrive) CompareFiles(string folderPath, List<String> driveFiles)
         {
             List<String> buffer = new List<string>();
@@ -238,6 +290,30 @@ namespace Google_Drive_data_scraper
             return (missingFilesFromFolder, missingFilesFromDrive);
         }
 
+
+        /// <summary>
+        /// Saves the list of the files in the user's Drive in a txt file
+        /// </summary>
+        static void SaveList()
+        {
+            string filesNameTxtPath = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\GoogleDriveFiles.txt";
+            using (StreamWriter sw = File.CreateText(filesNameTxtPath))
+            {
+                int fileIndex = 1;
+                foreach (var name in fileNames)
+                {
+                    sw.WriteLine($"{fileIndex}) {name}");
+                    fileIndex++;
+                }
+            }
+            Process.Start(@"cmd.exe ", $"/c {Path.GetFullPath(filesNameTxtPath)}");
+        }
+
+
+        /// <summary>
+        /// Prints an error after changing the foreground color to red
+        /// </summary>
+        /// <param name="errorMessage"> Message to print </param>
         private static void PrintError(String errorMessage)
         {
             Console.ForegroundColor = ConsoleColor.Red;
@@ -245,6 +321,5 @@ namespace Google_Drive_data_scraper
             Console.ForegroundColor = ConsoleColor.White;
             Console.ReadLine();
         }
-
     }
 }
